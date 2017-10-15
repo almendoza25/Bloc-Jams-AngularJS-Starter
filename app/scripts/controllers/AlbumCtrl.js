604fbd41ae8197a6ba75a0bd745bbc03830df988
Alfredo Mendoza <almendoza25@gmail.com>
2017-10-15 14:39:18 -0700

    Assignment 5 completed

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,12 @@
 (function() {
     function AlbumCtrl() {
-      this.albumData = [];
-      this.albumData.push(angular.copy(albumPicasso));
-
+        this.album = [];
+        this.album.push(angular.copy(albumPicasso));
+        this.albumData = [];
+        for (var i = 0; i < 5; i++) {
+        this.albumData.push(angular.copy(albumPicasso));
     }
+}
 
         angular
             .module('blocJams')
